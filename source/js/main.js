869fb28eff5ed5ace73b4770b17dfc7e6043927d
gerlinda137 <gerlinda137@gmail.com>
2020-09-21 18:49:43 +0300

    проблемы с прокруткой и дата атрибутом

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,8 @@
-let mySwiper = new Swiper('.swiper-container', {
+const commentsSlider = document.querySelector('.swiper-container');
+const saleSlider = document.querySelector('.sale-chairs__swiper-container');
+
+
+let mySwiper = new Swiper(commentsSlider, {
   // Optional parameters
   // direction: 'vertical',
 
@@ -13,4 +17,65 @@ let mySwiper = new Swiper('.swiper-container', {
     prevEl: '.swiper-button-prev',
   },
 
+})
+
+
+// let mySwiper1 = new Swiper(saleSlider, {
+//   // Optional parameters
+//   // direction: 'vertical',
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+// })
+
+let mySwiper1;
+
+function mobileSlider() {
+  if (window.innerWidth <= 750 && saleSlider.dataset.mobile == 'false') {
+    mySwiper1 = new Swiper(saleSlider, {
+      // Optional parameters
+      // direction: 'vertical',
+
+      // If we need pagination
+      slidesPerView: 1,
+      centeredSlides: true,
+      spaceBetween: 20,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      slideClass: 'sale-card',
+
+    })
+    saleSlider.dataset.mobile == 'true';
+  }
+
+  if (window.innerWidth > 750) {
+    saleSlider.dataset.mobile = 'false';
+
+    if (saleSlider.classList.contains('swiper-container-initialized')) {
+      mySwiper1.destroy();
+    }
+
+  }
+}
+
+mobileSlider();
+window.addEventListener('resize', () => {
+  mobileSlider();
 })
